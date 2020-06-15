6dfa8b5c65e7855f635b8458563bfa694c34983e
cooper <lyt3131@163.com>
2020-06-15 15:27:38 +0800

    chang docke port

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  clientId: 'register',
+  clientId: 'archives',
   clientPassword: 'thisissecret',
   title: '建设工程审批系统',
   apiUrl: window["env"]["apiUrl"] || "http://192.168.1.21:5555",
